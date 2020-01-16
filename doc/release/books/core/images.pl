528a5e584f33853532a9677f2756f1f27f61e450
Matt McCormick <matt.mccormick@kitware.com>
2020-01-15 21:46:42 -0500

    Add Doxygen documentation from public.kitware.com

---
+++ doc/release/books/core/images.pl
@@ -0,0 +1,350 @@
+# LaTeX2HTML 2002-2-1 (1.71)
+# Associate images original text with physical files.
+
+
+$key = q/z;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="11" HEIGHT="12" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="$z$">|; 
+
+$key = q/x;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="12" HEIGHT="12" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="$x$">|; 
+
+$key = q/2^@{-ycs@};MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="65" HEIGHT="33" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img23.png"
+ ALT="$2^@{-ycs@}$">|; 
+
+$key = q/x^3,x^2y,...,y^3,...1;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="115" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img42.png"
+ ALT="$x^3, x^2y, ..., y^3, ... 1$">|; 
+
+$key = q/USV'=M;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="77" HEIGHT="17" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img18.png"
+ ALT="$U S V' = M$">|; 
+
+$key = q/P,Q,R,S;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="66" HEIGHT="28" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img45.png"
+ ALT="$P, Q, R, S$">|; 
+
+$key = q/[-1,@1];MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="56" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img46.png"
+ ALT="$[-1,@ 1]$">|; 
+
+$key = q/n^2;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="19" HEIGHT="18" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img57.png"
+ ALT="$n^2$">|; 
+
+$key = q/p;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="11" HEIGHT="27" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img36.png"
+ ALT="$p$">|; 
+
+$key = q/b;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="10" HEIGHT="13" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img8.png"
+ ALT="$b$">|; 
+
+$key = q/n;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="12" HEIGHT="12" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img10.png"
+ ALT="$n$">|; 
+
+$key = q/t;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="9" HEIGHT="12" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img34.png"
+ ALT="$t$">|; 
+
+$key = q/K;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="17" HEIGHT="16" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img40.png"
+ ALT="$K$">|; 
+
+$key = q/[0@0@0@1];MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="76" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img41.png"
+ ALT="$[0 @ 0 @ 0 @ 1]$">|; 
+
+$key = q/e';MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="14" HEIGHT="17" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img49.png"
+ ALT="$e'$">|; 
+
+$key = q/e,e';MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="28" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img50.png"
+ ALT="$e, e'$">|; 
+
+$key = q/(0,0,0)^t=[3@times4]@(C_x,C_y,C_z,1)^t;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="258" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img28.png"
+ ALT="$(0, 0, 0)^t = [3@times4]@ (C_x, C_y, C_z, 1)^t$">|; 
+
+$key = q/x=(1,2,10)^t;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="92" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img31.png"
+ ALT="$x = (1, 2, 10)^t$">|; 
+
+$key = q/R;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="15" HEIGHT="16" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img33.png"
+ ALT="$R$">|; 
+
+$key = q/B'A';MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="35" HEIGHT="17" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img15.png"
+ ALT="$B' A'$">|; 
+
+$key = q/2^@{-xcs@};MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="66" HEIGHT="33" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img22.png"
+ ALT="$2^@{-xcs@}$">|; 
+
+$key = q/C;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="15" HEIGHT="16" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img16.png"
+ ALT="$C$">|; 
+
+$key = q/C'=B'A';MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="71" HEIGHT="17" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img17.png"
+ ALT="$C' =
+B' A'$">|; 
+
+$key = q/O(n^3);MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="42" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img11.png"
+ ALT="$O(n^3)$">|; 
+
+$key = q/ax+by+c=0;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="104" HEIGHT="28" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img5.png"
+ ALT="$a x + b y + c = 0$">|; 
+
+$key = q/x';MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="16" HEIGHT="17" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img48.png"
+ ALT="$x'$">|; 
+
+$key = q/F;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="15" HEIGHT="16" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img51.png"
+ ALT="$F$">|; 
+
+$key = q/(u,v,1)^t=[3@times4]@X;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="175" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img29.png"
+ ALT="$(u, v, 1)^t = [3@times4]@ X$">|; 
+
+$key = q/B;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="15" HEIGHT="16" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img14.png"
+ ALT="$B$">|; 
+
+$key = q/O(mn^2);MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="55" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img19.png"
+ ALT="$O(mn^2)$">|; 
+
+$key = q/l'=F@x;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="64" HEIGHT="17" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img52.png"
+ ALT="$l' = F@ x$">|; 
+
+$key = q/c;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="10" HEIGHT="12" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img9.png"
+ ALT="$c$">|; 
+
+$key = q/[3@times4];MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="76" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img26.png"
+ ALT="$[3@times4]$">|; 
+
+$key = q/O(n^2);MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="42" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img12.png"
+ ALT="$O(n^2)$">|; 
+
+$key = q/O(m^2n);MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="55" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img20.png"
+ ALT="$O(m^2n)$">|; 
+
+$key = q/@lambda;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="63" HEIGHT="13" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img27.png"
+ ALT="$@lambda$">|; 
+
+$key = q/x'^tFx=0;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="68" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img53.png"
+ ALT="$x'^tFx = 0$">|; 
+
+$key = q/[R|t];MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="32" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img39.png"
+ ALT="$[R\vert t]$">|; 
+
+$key = q/v={R(x,y,z)@overS(x,y,z)};MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="191" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img44.png"
+ ALT="$v = {R(x, y, z) @over S(x, y, z)}$">|; 
+
+$key = q/X=(x,y,z,1)^t;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="105" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img24.png"
+ ALT="$X = (x, y, z, 1)^t$">|; 
+
+$key = q/(u,v);MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="38" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img56.png"
+ ALT="$(u, v)$">|; 
+
+$key = q/A;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="14" HEIGHT="16" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img13.png"
+ ALT="$A$">|; 
+
+$key = q/a;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="11" HEIGHT="12" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img7.png"
+ ALT="$a$">|; 
+
+$key = q/X_i=s(RY_i+t);MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="108" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img54.png"
+ ALT="$X_i = s(RY_i +t)$">|; 
+
+$key = q/(@lambdau,@lambdav,@lambda)^t=[3@times4]@X;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="346" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img25.png"
+ ALT="$(@lambda u, @lambda v, @lambda)^t = [3@times4]@ X$">|; 
+
+$key = q/x=K[R|t]X;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="88" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img38.png"
+ ALT="$ x =
+K[R\vert t]X$">|; 
+
+$key = q/y;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="11" HEIGHT="27" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="$y$">|; 
+
+$key = q/u=0.1,v=0.2;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="104" HEIGHT="27" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img32.png"
+ ALT="$u=0.1, v = 0.2$">|; 
+
+$key = q/u={P(x,y,z)@overQ(x,y,z)};MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="194" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img43.png"
+ ALT="$ u = {P(x, y, z) @over Q(x, y,z)}$">|; 
+
+$key = q/(u,v,1)^t;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="57" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img30.png"
+ ALT="$(u, v, 1)^t$">|; 
+
+$key = q/X;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="17" HEIGHT="16" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img37.png"
+ ALT="$X$">|; 
+
+$key = q/s;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="10" HEIGHT="12" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img55.png"
+ ALT="$s$">|; 
+
+$key = q/C=-R^tt;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="69" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img35.png"
+ ALT="$C = -R^t t$">|; 
+
+$key = q/f;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="12" HEIGHT="28" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img6.png"
+ ALT="$f$">|; 
+
+$key = q/m>n;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="45" HEIGHT="27" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img21.png"
+ ALT="$m &gt; n$">|; 
+
+$key = q/l';MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="12" HEIGHT="17" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img47.png"
+ ALT="$l'$">|; 
+
+$key = q/f(x,y,z);MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="61" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img4.png"
+ ALT="$f(x,y,z)$">|; 
+
+1;
+
---
+++ doc/release/books/core/l2h_cache.pm
@@ -0,0 +1,383 @@
+
+$l2h_cache_key = q/$@lambda$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="63" HEIGHT="13" ALIGN="BOTTOM" BORDER="0"
+ SRC="book_27.png"
+ ALT="$@lambda$"></SPAN>|;
+
+$l2h_cache_key = q/$X = (x, y, z, 1)^t$/;
+$l2h_cache{$l2h_cache_key} = q|<!-- MATH
+ $X = (x, y, z, 1)^t$
+ -->
+<SPAN CLASS="MATH"><IMG
+ WIDTH="105" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_24.png"
+ ALT="$X = (x, y, z, 1)^t$"></SPAN>|;
+
+$l2h_cache_key = q/$C = -R^t t$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="69" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_35.png"
+ ALT="$C = -R^t t$"></SPAN>|;
+
+$l2h_cache_key = q/$R$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="15" HEIGHT="16" ALIGN="BOTTOM" BORDER="0"
+ SRC="book_33.png"
+ ALT="$R$"></SPAN>|;
+
+$l2h_cache_key = q/$b$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="10" HEIGHT="13" ALIGN="BOTTOM" BORDER="0"
+ SRC="book_8.png"
+ ALT="$b$"></SPAN>|;
+
+$l2h_cache_key = q/$x$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="12" HEIGHT="12" ALIGN="BOTTOM" BORDER="0"
+ SRC="book_1.png"
+ ALT="$x$"></SPAN>|;
+
+$l2h_cache_key = q/$[-1,@ 1]$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="56" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_46.png"
+ ALT="$[-1,@ 1]$"></SPAN>|;
+
+$l2h_cache_key = q/$v = {R(x, y, z) @over S(x, y, z)}$/;
+$l2h_cache{$l2h_cache_key} = q|<!-- MATH
+ $v = {R(x, y, z) @over S(x, y, z)}$
+ -->
+<SPAN CLASS="MATH"><IMG
+ WIDTH="191" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_44.png"
+ ALT="$v = {R(x, y, z) @over S(x, y, z)}$"></SPAN>|;
+
+$l2h_cache_key = q/$x'$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="16" HEIGHT="17" ALIGN="BOTTOM" BORDER="0"
+ SRC="book_48.png"
+ ALT="$x'$"></SPAN>|;
+
+$l2h_cache_key = q/$z$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="11" HEIGHT="12" ALIGN="BOTTOM" BORDER="0"
+ SRC="book_3.png"
+ ALT="$z$"></SPAN>|;
+
+$l2h_cache_key = q/$x^3, x^2y, ..., y^3, ... 1$/;
+$l2h_cache{$l2h_cache_key} = q|<!-- MATH
+ $x^3, x^2y, ..., y^3, ... 1$
+ -->
+<SPAN CLASS="MATH"><IMG
+ WIDTH="115" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_42.png"
+ ALT="$x^3, x^2y, ..., y^3, ... 1$"></SPAN>|;
+
+$l2h_cache_key = q/$B$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="15" HEIGHT="16" ALIGN="BOTTOM" BORDER="0"
+ SRC="book_14.png"
+ ALT="$B$"></SPAN>|;
+
+$l2h_cache_key = q/$n$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="12" HEIGHT="12" ALIGN="BOTTOM" BORDER="0"
+ SRC="book_10.png"
+ ALT="$n$"></SPAN>|;
+
+$l2h_cache_key = q/$O(m^2n)$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="55" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_20.png"
+ ALT="$O(m^2n)$"></SPAN>|;
+
+$l2h_cache_key = q/$B' A'$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="35" HEIGHT="17" ALIGN="BOTTOM" BORDER="0"
+ SRC="book_15.png"
+ ALT="$B' A'$"></SPAN>|;
+
+$l2h_cache_key = q/$e'$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="14" HEIGHT="17" ALIGN="BOTTOM" BORDER="0"
+ SRC="book_49.png"
+ ALT="$e'$"></SPAN>|;
+
+$l2h_cache_key = q/$O(n^2)$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="42" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_12.png"
+ ALT="$O(n^2)$"></SPAN>|;
+
+$l2h_cache_key = q/$(u, v)$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="38" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_56.png"
+ ALT="$(u, v)$"></SPAN>|;
+
+$l2h_cache_key = q/$(u, v, 1)^t$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="57" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_30.png"
+ ALT="$(u, v, 1)^t$"></SPAN>|;
+
+$l2h_cache_key = q/$2^@{-ycs@}$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="65" HEIGHT="33" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_23.png"
+ ALT="$2^@{-ycs@}$"></SPAN>|;
+
+$l2h_cache_key = q/$n^2$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="19" HEIGHT="18" ALIGN="BOTTOM" BORDER="0"
+ SRC="book_57.png"
+ ALT="$n^2$"></SPAN>|;
+
+$l2h_cache_key = q/$O(mn^2)$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="55" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_19.png"
+ ALT="$O(mn^2)$"></SPAN>|;
+
+$l2h_cache_key = q/$X$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="17" HEIGHT="16" ALIGN="BOTTOM" BORDER="0"
+ SRC="book_37.png"
+ ALT="$X$"></SPAN>|;
+
+$l2h_cache_key = q/$2^@{-xcs@}$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="66" HEIGHT="33" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_22.png"
+ ALT="$2^@{-xcs@}$"></SPAN>|;
+
+$l2h_cache_key = q/$x = (1, 2, 10)^t$/;
+$l2h_cache{$l2h_cache_key} = q|<!-- MATH
+ $x = (1, 2, 10)^t$
+ -->
+<SPAN CLASS="MATH"><IMG
+ WIDTH="92" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_31.png"
+ ALT="$x = (1, 2, 10)^t$"></SPAN>|;
+
+$l2h_cache_key = q/$x'^tFx = 0$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="68" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="book_53.png"
+ ALT="$x'^tFx = 0$"></SPAN>|;
+
+$l2h_cache_key = q/$O(n^3)$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="42" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_11.png"
+ ALT="$O(n^3)$"></SPAN>|;
+
+$l2h_cache_key = q/$K$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="17" HEIGHT="16" ALIGN="BOTTOM" BORDER="0"
+ SRC="book_40.png"
+ ALT="$K$"></SPAN>|;
+
+$l2h_cache_key = q/$l' = F@ x$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="64" HEIGHT="17" ALIGN="BOTTOM" BORDER="0"
+ SRC="book_52.png"
+ ALT="$l' = F@ x$"></SPAN>|;
+
+$l2h_cache_key = q/$a$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="11" HEIGHT="12" ALIGN="BOTTOM" BORDER="0"
+ SRC="book_7.png"
+ ALT="$a$"></SPAN>|;
+
+$l2h_cache_key = q/$[3@times4]$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="76" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_26.png"
+ ALT="$[3@times4]$"></SPAN>|;
+
+$l2h_cache_key = q/$c$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="10" HEIGHT="12" ALIGN="BOTTOM" BORDER="0"
+ SRC="book_9.png"
+ ALT="$c$"></SPAN>|;
+
+$l2h_cache_key = q/$(u, v, 1)^t = [3@times4]@ X$/;
+$l2h_cache{$l2h_cache_key} = q|<!-- MATH
+ $(u, v, 1)^t = [3@times4]@ X$
+ -->
+<SPAN CLASS="MATH"><IMG
+ WIDTH="175" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_29.png"
+ ALT="$(u, v, 1)^t = [3@times4]@ X$"></SPAN>|;
+
+$l2h_cache_key = q/$f(x,y,z)$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="61" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_4.png"
+ ALT="$f(x,y,z)$"></SPAN>|;
+
+$l2h_cache_key = q/$F$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="15" HEIGHT="16" ALIGN="BOTTOM" BORDER="0"
+ SRC="book_51.png"
+ ALT="$F$"></SPAN>|;
+
+$l2h_cache_key = q/$f$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="12" HEIGHT="28" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_6.png"
+ ALT="$f$"></SPAN>|;
+
+$l2h_cache_key = q/$e, e'$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="28" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_50.png"
+ ALT="$e, e'$"></SPAN>|;
+
+$l2h_cache_key = q/$(@lambda u, @lambda v, @lambda)^t = [3@times4]@ X$/;
+$l2h_cache{$l2h_cache_key} = q|<!-- MATH
+ $(@lambda u, @lambda v, @lambda)^t = [3@times4]@ X$
+ -->
+<SPAN CLASS="MATH"><IMG
+ WIDTH="346" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_25.png"
+ ALT="$(@lambda u, @lambda v, @lambda)^t = [3@times4]@ X$"></SPAN>|;
+
+$l2h_cache_key = q/$s$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="10" HEIGHT="12" ALIGN="BOTTOM" BORDER="0"
+ SRC="book_55.png"
+ ALT="$s$"></SPAN>|;
+
+$l2h_cache_key = q/$l'$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="12" HEIGHT="17" ALIGN="BOTTOM" BORDER="0"
+ SRC="book_47.png"
+ ALT="$l'$"></SPAN>|;
+
+$l2h_cache_key = q/$U S V' = M$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="77" HEIGHT="17" ALIGN="BOTTOM" BORDER="0"
+ SRC="book_18.png"
+ ALT="$U S V' = M$"></SPAN>|;
+
+$l2h_cache_key = q/$p$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="11" HEIGHT="27" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_36.png"
+ ALT="$p$"></SPAN>|;
+
+$l2h_cache_key = q/$a x + b y + c = 0$/;
+$l2h_cache{$l2h_cache_key} = q|<!-- MATH
+ $a x + b y + c = 0$
+ -->
+<SPAN CLASS="MATH"><IMG
+ WIDTH="104" HEIGHT="28" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_5.png"
+ ALT="$a x + b y + c = 0$"></SPAN>|;
+
+$l2h_cache_key = q/$y$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="11" HEIGHT="27" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_2.png"
+ ALT="$y$"></SPAN>|;
+
+$l2h_cache_key = q/$u=0.1, v = 0.2$/;
+$l2h_cache{$l2h_cache_key} = q|<!-- MATH
+ $u=0.1, v = 0.2$
+ -->
+<SPAN CLASS="MATH"><IMG
+ WIDTH="104" HEIGHT="27" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_32.png"
+ ALT="$u=0.1, v = 0.2$"></SPAN>|;
+
+$l2h_cache_key = q/$ x =
+K[R|t]X$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="88" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_38.png"
+ ALT="$ x =
+K[R\vert t]X$"></SPAN>|;
+
+$l2h_cache_key = q/$C' =
+B' A'$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="71" HEIGHT="17" ALIGN="BOTTOM" BORDER="0"
+ SRC="book_17.png"
+ ALT="$C' =
+B' A'$"></SPAN>|;
+
+$l2h_cache_key = q/$A$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="14" HEIGHT="16" ALIGN="BOTTOM" BORDER="0"
+ SRC="book_13.png"
+ ALT="$A$"></SPAN>|;
+
+$l2h_cache_key = q/$C$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="15" HEIGHT="16" ALIGN="BOTTOM" BORDER="0"
+ SRC="book_16.png"
+ ALT="$C$"></SPAN>|;
+
+$l2h_cache_key = q/$(0, 0, 0)^t = [3@times4]@ (C_x, C_y, C_z, 1)^t$/;
+$l2h_cache{$l2h_cache_key} = q|<!-- MATH
+ $(0, 0, 0)^t = [3@times4]@ (C_x, C_y, C_z, 1)^t$
+ -->
+<SPAN CLASS="MATH"><IMG
+ WIDTH="258" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_28.png"
+ ALT="$(0, 0, 0)^t = [3@times4]@ (C_x, C_y, C_z, 1)^t$"></SPAN>|;
+
+$l2h_cache_key = q/$P, Q, R, S$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="66" HEIGHT="28" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_45.png"
+ ALT="$P, Q, R, S$"></SPAN>|;
+
+$l2h_cache_key = q/$ u = {P(x, y, z) @over Q(x, y,z)}$/;
+$l2h_cache{$l2h_cache_key} = q|<!-- MATH
+ $u = {P(x, y, z) @over Q(x, y,z)}$
+ -->
+<SPAN CLASS="MATH"><IMG
+ WIDTH="194" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_43.png"
+ ALT="$ u = {P(x, y, z) @over Q(x, y,z)}$"></SPAN>|;
+
+$l2h_cache_key = q/$m > n$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="45" HEIGHT="27" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_21.png"
+ ALT="$m &gt; n$"></SPAN>|;
+
+$l2h_cache_key = q/$X_i = s(RY_i +t)$/;
+$l2h_cache{$l2h_cache_key} = q|<!-- MATH
+ $X_i = s(RY_i +t)$
+ -->
+<SPAN CLASS="MATH"><IMG
+ WIDTH="108" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_54.png"
+ ALT="$X_i = s(RY_i +t)$"></SPAN>|;
+
+$l2h_cache_key = q/$[0 @ 0 @ 0 @ 1]$/;
+$l2h_cache{$l2h_cache_key} = q|<!-- MATH
+ $[0 @ 0 @ 0 @ 1]$
+ -->
+<SPAN CLASS="MATH"><IMG
+ WIDTH="76" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_41.png"
+ ALT="$[0 @ 0 @ 0 @ 1]$"></SPAN>|;
+
+$l2h_cache_key = q/$[R|t]$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="32" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_39.png"
+ ALT="$[R\vert t]$"></SPAN>|;
+
+$l2h_cache_key = q/$t$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="9" HEIGHT="12" ALIGN="BOTTOM" BORDER="0"
+ SRC="book_34.png"
+ ALT="$t$"></SPAN>|;
+1;
