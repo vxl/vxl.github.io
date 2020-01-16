674f3d02f6783d3aee32f85564e1f28a3827f8c5
Matt McCormick <matt.mccormick@kitware.com>
2020-01-15 21:46:42 -0500

    Add Doxygen documentation from public.kitware.com

---
+++ doc/release/books/contrib/mul/images.pl
@@ -0,0 +1,24 @@
+# LaTeX2HTML 2002-2-1 (1.71)
+# Associate images original text with physical files.
+
+
+$key = q/|R^n;MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="27" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="$\vert R^n$">|; 
+
+$key = q/P(x'<x);MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="68" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="$P(x' &lt; x)$">|; 
+
+$key = q/0.5*(1+vcl_erf(xslashvcl_sqrt(2.0));MSF=1.5;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="212" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="$0.5*(1+vcl_erf(x/vcl_sqrt(2.0))$">|; 
+
+1;
+
---
+++ doc/release/books/contrib/mul/l2h_cache.pm
@@ -0,0 +1,22 @@
+
+$l2h_cache_key = q/$|R^n$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="27" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_3.png"
+ ALT="$\vert R^n$"></SPAN>|;
+
+$l2h_cache_key = q/$P(x' < x)$/;
+$l2h_cache{$l2h_cache_key} = q|<SPAN CLASS="MATH"><IMG
+ WIDTH="68" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_1.png"
+ ALT="$P(x' &lt; x)$"></SPAN>|;
+
+$l2h_cache_key = q/$0.5*(1+vcl_erf(x\/vcl_sqrt(2.0))$/;
+$l2h_cache{$l2h_cache_key} = q|<!-- MATH
+ $0.5*(1+vcl_erf(x/vcl_sqrt(2.0))$
+ -->
+<SPAN CLASS="MATH"><IMG
+ WIDTH="212" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
+ SRC="book_2.png"
+ ALT="$0.5*(1+vcl_erf(x/vcl_sqrt(2.0))$"></SPAN>|;
+1;
